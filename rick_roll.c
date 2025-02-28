b441074da97a24aca9f1e4d288950b50db031153
themysterysolver <arjunprabhakar1910@gmail.com>
2025-02-28 14:57:17 +0530

    FEAT:good but to FIX:line-by-line rendering

---
+++ rick_roll.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define LINE_COUNT 37
+#define MAX 1024
+int main(){
+    FILE *fp=fopen("rickroll.txt","r");
+    if(fp==NULL){
+        printf("FILE NOT FOUND!");
+        exit(1);
+    }
+    char buffer[MAX];
+    int count=0;
+    while(fgets(buffer,MAX,fp)){
+        if(count==0){
+            printf("\033[H\033[J");
+        }
+        count++;
+        printf("%s",buffer);
+        if(count==LINE_COUNT){
+            count=0;
+            usleep(100000);
+        }
+    }
+
+}
